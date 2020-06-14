567532b16e81631e330699ca9bd806c7ea468c28
ksdfg <ksdfg123@gmail.com>
2020-06-14 20:11:18 +0530

    add db_uri to config

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -3,17 +3,18 @@ from os import environ
 
 # class for configuration of a bot
 class Config:
-    def __init__(self, token):
+    def __init__(self, token, db_uri):
         """
         Function to initialize the config for a bot
         :param token: API Token of the bot
         """
 
         self.TOKEN = token
+        self.DB_URI = db_uri
 
 
 try:
-    config = Config(token=environ['TOKEN'])
+    config = Config(token=environ['TOKEN'], db_uri=environ['DATABASE_URL'])
 except KeyError:
     print("Please set all required environment variables")
     exit(1)
