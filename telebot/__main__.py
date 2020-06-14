c85d2ce194b5512c24df82982e74f0f8ca524cb0
ksdfg <ksdfg123@gmail.com>
2020-06-14 21:36:27 +0530

    add help function + function for logging activity

--- telebot/__main__.py
+++ telebot/__main__.py
@@ -1,39 +1,76 @@
 from collections import OrderedDict
 from random import choice
 
-from telegram.ext import CommandHandler
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
 
-from telebot import dispatcher, updater
+from telebot import dispatcher, updater, config
 
+# default reply strings
 
 START_TEXT = """
 Hello, everynyan!
 
-I'm `kawai neko chan`, a cute little bot that does nothing rn.
+I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
+"""
+
+HELP_TEXT = """
+Hello, everynyan!
+
+I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
+
+Use following commands to use me (*blush*):
+/help - recursion ftw
+/start - turn me on
+/talk - make me meow
 """
 
 
-def start(update, context):
+def log(username: str, func_name: str, extra_text: str = ""):
+    """
+    Function to log bot activity
+    :param username: username of the user who called a command
+    :param func_name: name of the function being called
+    :param extra_text: any extra text to be logged
+    :return: None
+    """
+    print("------------------------------------")
+    print(username, "called function", func_name)
+    if extra_text:
+        print(extra_text)
+
+
+def start(update: Update, context: CallbackContext):
     # start message
-    print(update.message.from_user.username, "called start function")
+    log(username=update.message.from_user.username, func_name="start")
     update.message.reply_markdown(START_TEXT)
 
 
-def talk(update, context):
+def help(update: Update, context: CallbackContext):
+    # display help message
+    log(username=update.message.from_user.username, func_name="help")
+    update.message.reply_markdown(HELP_TEXT)
+
+
+def talk(update: Update, context: CallbackContext):
     # this cat meows
-    print(update.message.from_user.username, "called talk function")
+    log(username=update.message.from_user.username, func_name="talk")
     update.message.reply_markdown(f"`{('meow ' * choice(range(100))).rstrip()}`")
 
 
 START_COMMAND_HANDLER = CommandHandler("start", start)
 TALK_COMMAND_HANDLER = CommandHandler("talk", talk)
+HELP_COMMAND_HANDLER = CommandHandler("help", help)
 
 
 if __name__ == "__main__":
     # add handlers
     dispatcher.add_handler(START_COMMAND_HANDLER)
     dispatcher.add_handler(TALK_COMMAND_HANDLER)
+    dispatcher.add_handler(HELP_COMMAND_HANDLER)
 
+    # start bot
     updater.start_polling()
+
     print("neko chan can meow now")
     updater.idle()
