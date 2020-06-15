d8d289322dd978d51888f01fa11178d25eda7fb4
ksdfg <ksdfg123@gmail.com>
2020-06-15 15:40:56 +0530

    parkour!
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -1,8 +1,7 @@
 from os import environ
-from random import choice
 
-from telegram import Update, BotCommand
-from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram import Update
+from telegram.ext import Updater
 
 
 # class for configuration of a bot
@@ -56,65 +55,3 @@ config = Config(
 # create updater and dispatcher
 updater = Updater(config.TOKEN, use_context=True)
 dispatcher = updater.dispatcher
-
-
-# default reply strings
-
-START_TEXT = """
-Hello, everynyan!
-
-I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
-"""
-
-HELP_TEXT = """
-Hello, everynyan!
-
-I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
-
-Use following commands to use me (*blush*):
-/help - Recursion ftw
-/start - Turn me on
-/talk - Make me meow
-"""
-
-
-def start(update: Update, context: CallbackContext):
-    # start message
-    log(update, func_name="start")
-    update.message.reply_markdown(START_TEXT)
-
-
-def help(update: Update, context: CallbackContext):
-    # display help message
-    log(update, func_name="help")
-    update.message.reply_markdown(HELP_TEXT)
-
-
-def talk(update: Update, context: CallbackContext):
-    # this cat meows
-    log(update, func_name="talk")
-
-    word = "meow "
-    if context.args:
-        word = context.args[0] + " "
-
-    spem = (word * choice(range(100))).rstrip()
-    if word != "meow ":
-        spem += "\n\nmeow"
-
-    update.message.reply_markdown(f"`{spem}`")
-
-
-# create handlers
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("talk", talk))
-dispatcher.add_handler(CommandHandler("help", help))
-
-
-# set bot commands
-COMMANDS = [
-    BotCommand(command='help', description="Display the help text to understand how to use this bot"),
-    BotCommand(command='talk', description="Say <word> (or meow, if not given) rendum number of times."),
-]
-
-updater.bot.set_my_commands(COMMANDS)
--- telebot/__main__.py
+++ telebot/__main__.py
@@ -1,6 +1,10 @@
 import importlib
+from random import choice
 
-from telebot import updater, config
+from telegram import Update, BotCommand
+from telegram.ext import CallbackContext, CommandHandler
+
+from telebot import updater, config, log, dispatcher
 from telebot.modules import ALL_MODULES
 
 # Import all modules
@@ -14,7 +18,68 @@ for module_name in ALL_MODULES:
 
 print("Imported modules :", sorted(IMPORTED), "\n")
 
+
+# default reply strings
+
+START_TEXT = """
+Hello, everynyan!
+
+I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
+"""
+
+HELP_TEXT = """
+Hello, everynyan!
+
+I'm `kawai neko chan`, a cute little bot that does rendum shit rn.
+
+Use following commands to use me (*blush*):
+/help - Recursion ftw
+/start - Turn me on
+/talk - Make me meow
+"""
+
+
+def start(update: Update, context: CallbackContext):
+    # start message
+    log(update, func_name="start")
+    update.message.reply_markdown(START_TEXT)
+
+
+def help(update: Update, context: CallbackContext):
+    # display help message
+    log(update, func_name="help")
+    update.message.reply_markdown(HELP_TEXT)
+
+
+def talk(update: Update, context: CallbackContext):
+    # this cat meows
+    log(update, func_name="talk")
+
+    word = "meow "
+    if context.args:
+        word = context.args[0] + " "
+
+    spem = (word * choice(range(100))).rstrip()
+    if word != "meow ":
+        spem += "\n\nmeow"
+
+    update.message.reply_markdown(f"`{spem}`")
+
+
+# set bot commands
+COMMANDS = [
+    BotCommand(command='help', description="Display the help text to understand how to use this bot"),
+    BotCommand(command='talk', description="Say <word> (or meow, if not given) rendum number of times."),
+]
+
 if __name__ == "__main__":
+    # create handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("talk", talk))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    updater.bot.set_my_commands(COMMANDS)
+
     # start bot
     if config.WEBHOOK_URL:
         updater.start_webhook(listen="0.0.0.0", port=config.PORT, url_path=config.TOKEN)
