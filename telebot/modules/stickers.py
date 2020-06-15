5456f8204c94cbf61e5401d31ef15998c065059f
ksdfg <ksdfg123@gmail.com>
2020-06-15 15:45:23 +0530

    add get_sticker to stickers module
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/stickers.py
+++ telebot/modules/stickers.py
@@ -1,3 +1,5 @@
+from os import remove
+
 from telegram import Update
 from telegram.ext import run_async, CallbackContext, CommandHandler
 from telegram.utils.helpers import escape_markdown
@@ -14,10 +16,33 @@ def sticker_id(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Please reply to a sticker to get its ID.")
 
 
+@run_async
+def get_sticker(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    chat_id = update.effective_chat.id
+
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        # download file
+        file_id = msg.reply_to_message.sticker.file_id
+        new_file = context.bot.get_file(file_id)
+        new_file.download('sticker.png')
+
+        # send picture
+        context.bot.send_photo(chat_id, photo=open('sticker.png', 'rb'))
+
+        # delete locally created image
+        remove("sticker.png")
+
+    else:
+        update.effective_message.reply_text("Please reply to a sticker for me to upload its PNG.")
+
+
 __help__ = """
 - /stickerid: reply to a sticker to me to tell you its file ID.
+- /getsticker: reply to a sticker to me to upload its raw PNG file.
 """
 
 __mod_name__ = "Stickers"
 
 dispatcher.add_handler(CommandHandler("stickerid", sticker_id))
+dispatcher.add_handler(CommandHandler("getsticker", get_sticker))
