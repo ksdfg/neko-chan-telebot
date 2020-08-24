25309cc7c73484572e36430f947e2e54040e3e3d
ksdfg <ksdfg123@gmail.com>
2020-08-24 12:19:46 +0530

    opencv error in deeppyer library...
    Co-authored-by: Rhyse Simpson <skittles9832@gmail.com>
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/memes.py
+++ telebot/modules/memes.py
@@ -1,8 +1,16 @@
+from asyncio import new_event_loop
+from io import BytesIO
+from os import remove
+from pathlib import Path
+from random import choice
 from random import choice, randint
 from re import sub
 
+from PIL import Image
+from deeppyer import deepfry
+from emoji import emojize
 from spongemock.spongemock import mock as mock_text
-from telegram import Update
+from telegram import Update, Message
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from zalgo_text.zalgo import zalgo
@@ -172,12 +180,49 @@ def vapor(update: Update, context: CallbackContext):
         update.effective_message.reply_to_message.reply_markdown(f"`{aesthetic_text}`")
 
 
+async def _fry(image: Image, msg: Message):
+    image = await deepfry(img=image)
+
+    bio = BytesIO()
+    bio.name = 'image.jpeg'
+    image.save(bio, 'JPEG')
+
+    bio.seek(0)
+    msg.reply_photo(bio)
+    if Path("sticker.png").is_file():
+        remove("sticker.png")
+
+
+@run_async
+def fry(update: Update, context: CallbackContext):
+    log(update, "deepfry")
+
+    if update.effective_message.reply_to_message and update.effective_message.reply_to_message.photo:
+        image = Image.open(update.effective_message.reply_to_message.photo[-1].get_file().download_as_bytearray())
+    elif update.effective_message.reply_to_message and update.effective_message.reply_to_message.sticker:
+        context.bot.get_file(update.effective_message.reply_to_message.sticker.file_id).download('sticker.png')
+        image = Image.open("sticker.png")
+    else:
+        update.effective_message.reply_text(
+            emojize("Gimme something proper to deepfry before I deepfry your catnip :pouting_cat_face:")
+        )
+        return
+
+    loop = new_event_loop()
+    loop.run_until_complete(_fry(image, update.effective_message.reply_to_message))
+    loop.close()
+
+
 __help__ = """
 - /mock `<reply>` : MoCk LikE sPOnGEbob
 - /zalgofy `<reply>` : cͩ͠o̴͕r͌̈ȓ͡ṵ̠p̟͜tͯ͞ t̷͂ḣ͞ȩ͗ t̪̉e̢̪x̨͑t̼ͨ
 - /owo `<reply>` : translate normie to moe weeb
 - /stretch `<reply>` : talk like the sloth from zootopia
 - /vapor `[<reply>|<message>]` : ｖａｐｏｒｗａｖｅ ａｅｓｔｈｅｔｉｃｓ
+/mock <reply> : MoCk LikE sPOnGEbob
+/zalgofy <reply> : cͩ͠o̴͕r͌̈ȓ͡ṵ̠p̟͜tͯ͞ t̷͂ḣ͞ȩ͗ t̪̉e̢̪x̨͑t̼ͨ
+/owo <reply> : translate normie to moe weeb
+/deepfry <reply photo|sticker> : deepfried memes for the american kitties
 """
 
 __mod_name__ = "memes"
@@ -188,3 +233,4 @@ dispatcher.add_handler(CommandHandler("zalgofy", zalgofy))
 dispatcher.add_handler(CommandHandler("owo", owo))
 dispatcher.add_handler(CommandHandler("stretch", stretch))
 dispatcher.add_handler(CommandHandler("vapor", vapor))
+dispatcher.add_handler(CommandHandler("deepfry", fry))
