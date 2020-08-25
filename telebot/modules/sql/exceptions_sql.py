b17979cb47e5b18b0b9a948d23c1f2bf59a913e3
ksdfg <ksdfg123@gmail.com>
2020-08-25 10:11:30 +0530

    proper documentation in exceptions_sql.py
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/sql/exceptions_sql.py
+++ telebot/modules/sql/exceptions_sql.py
@@ -1,19 +1,25 @@
 from copy import deepcopy
+from typing import List
 
 from mongoengine import Document, StringField, ListField, IntField
 
 
 # create models for this module
 class Exceptions(Document):
-    command = StringField(required=True)
-    chats = ListField(IntField())
+    command = StringField(required=True)  # command for which exception is being set
+    chats = ListField(IntField())  # list of chat IDs in which exception is being set
 
 
 # create helper functions to be used to interact with the model
 
 
-def get_command_exception_chats(command: str) -> list:
-    exceptions = Exceptions.objects(command=command)
+def get_command_exception_chats(command: str) -> List[int]:
+    """
+    Get all chats that have an exception for a given command
+    :param command: the command whose exception we're checking for
+    :return: list of chat IDs
+    """
+    exceptions: List[Exceptions] = Exceptions.objects(command=command)
 
     if exceptions:
         return exceptions[0].chats
@@ -22,16 +28,24 @@ def get_command_exception_chats(command: str) -> list:
 
 
 def add_command_exception_chats(command: str, chat: int) -> str:
-    exceptions_list = Exceptions.objects(command=command)
+    """
+    Add exception for a command in a chat
+    :param command: the command for which we're adding an exception
+    :param chat: the chat ID in which we're adding the exception
+    :return: the message to be replied to the user in Telegram
+    """
+    exceptions_list: List[Exceptions] = Exceptions.objects(command=command)
 
+    # if exceptions document for command exists
     if exceptions_list:
-        exceptions = exceptions_list[0]
+        exceptions: Exceptions = exceptions_list[0]
         if chat in exceptions.chats:
             return f"Exception for command `{command}` already added!"
         else:
-            exceptions.chats = exceptions.chats + [chat]
+            exceptions.chats: List[int] = exceptions.chats + [chat]
             exceptions.save()
 
+    # if exceptions document doesn't exist for the command
     else:
         Exceptions(command=command, chats=[chat]).save()
 
@@ -39,30 +53,47 @@ def add_command_exception_chats(command: str, chat: int) -> str:
 
 
 def del_command_exception_chats(command: str, chat: int) -> str:
-    exceptions_list = Exceptions.objects(command=command)
+    """
+    delete exception for a command in a chat
+    :param command: the command for which we're deleting an exception
+    :param chat: the chat ID in which we're deleting the exception
+    :return: the message to be replied to the user in Telegram
+    """
+    exceptions_list: List[Exceptions] = Exceptions.objects(command=command)
 
     if exceptions_list:
-        exceptions = exceptions_list[0]
+        exceptions: Exceptions = exceptions_list[0]
+
+        # delete exception
         try:
-            chats = deepcopy(exceptions.chats)
+            chats: List[int] = deepcopy(exceptions.chats)
             chats.remove(chat)
             if chats:
+                # if there are other chats in which this exception exists
                 exceptions.chats = chats
                 exceptions.save()
             else:
+                # if there are no other chats in which this exception exists
                 exceptions.delete()
 
             return f"Exception for command `{command}` deleted!"
 
         except ValueError:
+            # if the given chat ID isn't in the list of chats with exception for given command
             return f"Exception for command `{command}` not added!"
 
     else:
+        # if there is no exceptions document for this command
         return f"Exception for command `{command}` not added!"
 
 
-def get_exceptions_for_chat(chat: int) -> list:
-    exceptions = Exceptions.objects(chats=chat)
+def get_exceptions_for_chat(chat: int) -> List[str]:
+    """
+    Get all the commands for which an exception is added in a chat
+    :param chat: chat ID whose exceptions we're fetching
+    :return: list of all the commands for which an exception is added
+    """
+    exceptions: List[Exceptions] = Exceptions.objects(chats=chat)
 
     if exceptions:
         return [x.command for x in exceptions]
