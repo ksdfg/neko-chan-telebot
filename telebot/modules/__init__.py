ef4adfd805c4fba5adf0b797b9f93529cbe1b48c
ksdfg <ksdfg123@gmail.com>
2020-08-25 10:40:17 +0530

    code simplification and proper documentation in modules __init__.py
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/__init__.py
+++ telebot/modules/__init__.py
@@ -1,12 +1,8 @@
-def _list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
+import glob
+from os.path import dirname, basename, isfile
 
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [
-        basename(f)[:-3] for f in mod_paths if isfile(f) and f.endswith(".py") and not f.endswith('__init__.py')
-    ]
-    return all_modules
-
-
-ALL_MODULES = sorted(_list_all_modules())
+# get names of all the module .py files currently in the repo
+mod_paths = glob.glob(dirname(__file__) + "/*.py")
+ALL_MODULES = sorted(
+    [basename(f)[:-3] for f in mod_paths if isfile(f) and f.endswith(".py") and not f.endswith('__init__.py')]
+)
