b95869f4664a436ea75c63b21f2db824b5866cff
ksdfg <ksdfg123@gmail.com>
2020-09-02 15:53:14 +0530

    add file with mod_name and help to map out what needs to be done
    Co-authored-by: Rhyse Simpson <skittles9832@gmail.com>
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

---
+++ telebot/modules/mute.py
@@ -0,0 +1,7 @@
+__help__ = """
+***Admin only :***
+- /mute `<userhandle> [x<m|h|d>]`: mutes a user for x time, or until they are unmuted. (via handle, or reply). m = minutes, h = hours, d = days.
+- /unmute `<userhandle>`: unmutes a user. Can also be used as a reply, muting the replied to user.
+"""
+
+__mod_name__ = "Muting"
