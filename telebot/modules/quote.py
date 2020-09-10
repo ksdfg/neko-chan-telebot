621174b452a5e08d5fb690c8efbfc7d2cedfa77b
alr0cks <alok.bhawankar@gmail.com>
2020-09-10 22:51:13 +0530

    Create temp Profile Pic for users who don't have it

--- telebot/modules/quote.py
+++ telebot/modules/quote.py
@@ -14,14 +14,37 @@ def get_sticker(update: Update, context: CallbackContext):
     context.bot.send_chat_action(update.effective_chat.id, 'upload_photo')
     rep_msg = update.effective_message.reply_to_message
 
+    def generate_temp_profile(name, assigned_color):
+        BASE_DIR = Path(__file__).parent.absolute()
+        ls = name.split(" ")
+        if len(ls) > 1:
+            initials = ls[0][0] + ls[1][0]
+        else:
+            initials = ls[0][0]
+
+        font_bold = ImageFont.truetype(join(BASE_DIR, 'Fonts', "LucidaGrandeBold.ttf"), size=60, encoding="unic")
+        img = Image.new("RGB", (160, 160), color=(assigned_color))
+        draw = ImageDraw.Draw(img)
+        text_width, text_height = draw.textsize(initials, font_bold)
+        position = ((160 - text_width) / 2, (160 - text_height) / 2)
+        draw.text(position, initials, (255, 255, 255), font=font_bold)
+        img.save(
+            f"{update.effective_message.reply_to_message.from_user.id}_dp.jpg",
+        )
+
     def get_message_data(rep_msg):  # Get required data of the message to be quoted
 
-        profile_pic = update.effective_message.reply_to_message.from_user.get_profile_photos().photos[0][
-            0
-        ]  # Get users Profile Photo
-
-        file_pp = context.bot.getFile(profile_pic)
-        file_pp.download(f"{update.effective_message.reply_to_message.from_user.id}_dp.jpg")
+        head_tg = [
+            (238, 73, 40),
+            (65, 169, 3),
+            (224, 150, 2),
+            (15, 148, 237),
+            (143, 59, 247),
+            (252, 67, 128),
+            (0, 161, 196),
+            (235, 112, 2),
+        ]
+        assigned_color = random.choice(head_tg)
 
         if rep_msg.from_user.last_name:
             name = rep_msg.from_user.first_name + " " + rep_msg.from_user.last_name
@@ -33,9 +56,20 @@ def get_sticker(update: Update, context: CallbackContext):
         if rep_msg.text:
             text = rep_msg.text
 
-        return name, text
+        try:
+            profile_pic = update.effective_message.reply_to_message.from_user.get_profile_photos().photos[0][
+                0
+            ]  # Get users Profile Photo
 
-    def get_raw_sticker(name, text):
+            file_pp = context.bot.getFile(profile_pic)
+            file_pp.download(f"{update.effective_message.reply_to_message.from_user.id}_dp.jpg")
+
+        except IndexError:
+            generate_temp_profile(name, assigned_color)
+
+        return name, text, assigned_color
+
+    def get_raw_sticker(name, text, assigned_color):
         def text_wrap(text: str, font: ImageFont, max_width: int) -> str:
             """
             Get properly formatted text
@@ -77,7 +111,7 @@ def get_sticker(update: Update, context: CallbackContext):
             img.putalpha(alpha)
             return img
 
-        def draw_text(name, text):
+        def draw_text(name, text, assigned_color):
             max_width = 400
 
             # create the ImageFont instances
@@ -91,17 +125,6 @@ def get_sticker(update: Update, context: CallbackContext):
             line_width_bold, line_height_bold = font_bold.getsize(name)
             line_width, line_height = font_normal.getsize_multiline(text_blob)
 
-            head_tg = [
-                (238, 73, 40),
-                (65, 169, 3),
-                (224, 150, 2),
-                (15, 148, 237),
-                (143, 59, 247),
-                (252, 67, 128),
-                (0, 161, 196),
-                (235, 112, 2),
-            ]
-
             # get scalable width and height
             img = Image.new(
                 "RGB",
@@ -114,7 +137,7 @@ def get_sticker(update: Update, context: CallbackContext):
             draw = ImageDraw.Draw(img)
 
             # put username
-            draw.text((20, 25), name, random.choice(head_tg), font_bold)
+            draw.text((20, 25), name, assigned_color, font_bold)
 
             # put text
             draw.multiline_text((20, 15 + line_height_bold + 45), text_blob, (255, 255, 255), font_normal)
@@ -163,11 +186,11 @@ def get_sticker(update: Update, context: CallbackContext):
         # get base directory
         BASE_DIR = Path(__file__).parent.absolute()
         dp = get_ico_thumbnail(f"{update.effective_message.reply_to_message.from_user.id}_dp.jpg")
-        body = draw_text(name, text)
+        body = draw_text(name, text, assigned_color)
         get_concat_h(dp, body)
 
-    name, text = get_message_data(rep_msg)
-    get_raw_sticker(name, text)
+    name, text, assigned_color = get_message_data(rep_msg)
+    get_raw_sticker(name, text, assigned_color)
 
     # send generated image as sticker
     rep_msg.reply_sticker(sticker=open(f"{update.effective_message.reply_to_message.from_user.id}_final.webp", "rb"))
