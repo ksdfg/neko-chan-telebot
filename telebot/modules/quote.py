75d297888ed8898985690a6559a26b1c6392935c
alr0cks <alok.bhawankar@gmail.com>
2020-09-12 23:27:04 +0530

    documentation

--- telebot/modules/quote.py
+++ telebot/modules/quote.py
@@ -26,24 +26,26 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
 
     def generate_temp_profile(name, assigned_color):
         """
-
-        :param name:
-        :param assigned_color:
-        :return:
+        generate temporary profile picture
+        :param name: username
+        :param assigned_color: random color assigned to user similar to telegram
         """
+        # Fetch Initials of full name
         BASE_DIR = Path(__file__).parent.absolute()
         ls = name.split(" ")
         if len(ls) > 1:
             initials = ls[0][0] + ls[1][0]
         else:
             initials = ls[0][0]
-
+        # Generate a temp profile picture similar to telegram
         font_bold = ImageFont.truetype(join(BASE_DIR, 'Fonts', "LucidaGrandeBold.ttf"), size=60, encoding="unic")
         img = Image.new("RGB", (160, 160), color=(assigned_color))
         draw = ImageDraw.Draw(img)
+        # put the initials in centre of image
         text_width, text_height = draw.textsize(initials, font_bold)
         position = ((160 - text_width) / 2, (160 - text_height) / 2)
         draw.text(position, initials, (255, 255, 255), font=font_bold)
+        # save the image
         img.save(
             f"{file_name}_dp.jpg",
         )
@@ -51,9 +53,10 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
     def get_message_data(rep_msg: Message) -> Tuple[str, str, Tuple[int, int, int]]:
         """
         Get required data of the message to be quoted
-        :param rep_msg:
-        :return:
+        :param rep_msg: object representing replied message information
+        :return: name of user, message to be quoted and assigned color
         """
+        # List consisting tuples of RGB for random color generation similar to telegram
         head_tg = [
             (238, 73, 40),
             (65, 169, 3),
@@ -65,6 +68,8 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
             (235, 112, 2),
         ]
         assigned_color = random.choice(head_tg)
+
+        # Get Name and message quote
 
         if rep_msg.from_user.last_name:
             name = rep_msg.from_user.first_name + " " + rep_msg.from_user.last_name
@@ -90,15 +95,14 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
 
     def get_raw_sticker(name: str, text: str, assigned_color: Tuple[int, int, int]):
         """
-
-        :param name:
-        :param text:
-        :param assigned_color:
-        :return:
+        :param name: Username of the quoted message
+        :param text: Message info to be quoted
+        :param assigned_color: random color assigned to the user similar to telegram
         """
 
         def text_wrap(text: str, font: ImageFont, max_width: int) -> str:
             """
+            Function to determine the number of lines the text is divided in.
             Get properly formatted text
             :param text: The base message text
             :param font: font to be used for the format
@@ -126,16 +130,18 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
 
         def add_corners(img: Image) -> Image:
             """
-
-            :param img:
-            :return:
+            apply rounded corners
+            :param img: Generated image
+            :return: Rounded corner image
             """
             rad = 30
+            # generate new circular image
             circle = Image.new("L", (rad * 2, rad * 2), 0)
             draw = ImageDraw.Draw(circle)
             draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
             alpha = Image.new("L", img.size, 255)
             w, h = img.size
+            # determine the rounded radius and edit image accordingly to all corners
             alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
             alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
             alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
@@ -145,11 +151,11 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
 
         def draw_text(name: str, text: str, assigned_color: Tuple[int, int, int]) -> Image:
             """
-
-            :param name:
-            :param text:
-            :param assigned_color:
-            :return:
+            create raw image for text
+            :param name: Name of user
+            :param text: Message to be quoted
+            :param assigned_color: random color assigned to the user similar to telegram
+            :return: raw image consisting of quoted text
             """
             max_width = 400
 
@@ -181,6 +187,7 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
             # put text
             draw.multiline_text((20, 15 + line_height_bold + 45), text_blob, (255, 255, 255), font_normal)
 
+            # add rounded corners
             result = add_corners(img)
 
             return result
@@ -188,9 +195,9 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
         def mask_circle_transparent(img: Image, offset: int = 0) -> Image:
             """
             mask the background of circular profile pic thumbnail
-            :param img:
-            :param offset:
-            :return:
+            :param img: Profile picture to convert to circular shape
+            :param offset: determining the shape of image
+            :return: circular and transparent background profile picture
             """
             offset = 0 * 2 + offset
             mask = Image.new("L", img.size, 0)
@@ -199,6 +206,7 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
                 (offset, offset, img.size[0] - offset, img.size[1] - offset),
                 fill=255,
             )
+            # apply transparency to background
             mask = mask.filter(ImageFilter.GaussianBlur(0))
 
             result = img.copy()
@@ -209,8 +217,8 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
         def get_ico_thumbnail(dp_name: str) -> Image:
             """
             get circular profile pic
-            :param dp_name:
-            :return:
+            :param dp_name: rectangular 160x160 profile picture
+            :return: circular profile picture thumbnail
             """
             im = Image.open(dp_name)
             size = 100, 100
@@ -220,21 +228,21 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
 
         def get_concat_h(img1: Image, img2: Image):
             """
-
-            :param img1:
-            :param img2:
-            :return:
+            join 2 images to create a quoted image similar to telegram
+            :param img1: Circular profile picture thumbnail
+            :param img2: Image consisting text message and name of the user
             """
             # concat both images
             dst = Image.new("RGB", (img1.width + img2.width + 15, max(img1.height, img2.height)))
             dst.putalpha(0)
             dst.paste(img1, (0, 0))
             dst.paste(img2, (img1.width + 15, 0))
+            # rebase image to fit into telegram's sticker's dimension rules
             basewidth = 512
             wpercent = basewidth / float(dst.size[0])
             hsize = int((float(dst.size[1]) * float(wpercent)))
             dst = dst.resize((basewidth, hsize), Image.ANTIALIAS)
-            # save image in webp format
+            # save image in png format
             dst.save(
                 f"{file_name}_final.png",
                 "PNG",
@@ -253,6 +261,7 @@ def _message_to_sticker(update: Update, context: CallbackContext) -> str:
     name, text, assigned_color = get_message_data(rep_msg)
     get_raw_sticker(name, text, assigned_color)
 
+    # delete saved profile picture from local storage
     if isfile(f"{file_name}_dp.jpg"):
         remove(f"{file_name}_dp.jpg")
 
@@ -283,8 +292,8 @@ def quote(update: Update, context: CallbackContext):
     update.effective_message.reply_sticker(sticker=open(file_name, "rb"))
 
     # remove stored image
-    if isfile(file_name):
-        remove(file_name)
+    # if isfile(file_name):
+    #     remove(file_name)
 
 
 __help__ = """
