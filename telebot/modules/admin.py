72cf057504d6fb569443448b38e4488720ddfc33
ksdfg <ksdfg123@gmail.com>
2020-10-09 23:03:22 +0530

    use the run_async parameter of Handler instead of decorator in admin.py
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/admin.py
+++ telebot/modules/admin.py
@@ -114,7 +114,6 @@ def get_datetime_form_args(args: List[str], username: str = "") -> Optional[date
     return until_date
 
 
-@run_async
 @bot_action("mute")
 @for_chat_types('supergroup')
 @check_user_admin
@@ -192,7 +191,6 @@ def mute(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Couldn't save record in db....")
 
 
-@run_async
 @bot_action("un mute")
 @for_chat_types('supergroup')
 @check_user_admin
@@ -295,7 +293,6 @@ def ban_kick(update: Update, context: CallbackContext):
     update.effective_message.reply_markdown(emojize(reply))
 
 
-@run_async
 @bot_action("ban")
 @for_chat_types('supergroup', 'channel')
 @check_user_admin
@@ -310,7 +307,6 @@ def ban(update: Update, context: CallbackContext):
     ban_kick(update, context)
 
 
-@run_async
 @bot_action("kick")
 @for_chat_types('supergroup', 'channel')
 @check_user_admin
@@ -325,7 +321,6 @@ def kick(update: Update, context: CallbackContext):
     ban_kick(update, context)
 
 
-@run_async
 @bot_action("promote")
 @for_chat_types('supergroup', 'channel')
 @check_user_admin
@@ -403,7 +398,6 @@ def promote(update: Update, context: CallbackContext):
         )
 
 
-@run_async
 @bot_action("demote")
 @for_chat_types('supergroup', 'channel')
 @check_user_admin
@@ -477,7 +471,6 @@ def demote(update: Update, context: CallbackContext):
         )
 
 
-@run_async
 @bot_action("pin")
 @for_chat_types('supergroup', 'channel')
 @check_bot_admin
@@ -534,7 +527,6 @@ def pin(update: Update, context: CallbackContext):
     )
 
 
-@run_async
 @bot_action("purge")
 @check_user_admin
 @check_bot_admin
@@ -613,11 +605,11 @@ If you add an exception to `admin`, I will allow admins to execute commands even
 __mod_name__ = "Admin"
 
 # create handlers
-dispatcher.add_handler(CommandHandler("promote", promote))
-dispatcher.add_handler(CommandHandler("demote", demote))
-dispatcher.add_handler(CommandHandler("mute", mute))
-dispatcher.add_handler(CommandHandler("unmute", unmute))
-dispatcher.add_handler(CommandHandler("ban", ban))
-dispatcher.add_handler(CommandHandler("kick", kick))
-dispatcher.add_handler(CommandHandler("pin", pin))
-dispatcher.add_handler(CommandHandler("purge", purge))
+dispatcher.add_handler(CommandHandler("promote", promote, run_async=True))
+dispatcher.add_handler(CommandHandler("demote", demote, run_async=True))
+dispatcher.add_handler(CommandHandler("mute", mute, run_async=True))
+dispatcher.add_handler(CommandHandler("unmute", unmute, run_async=True))
+dispatcher.add_handler(CommandHandler("ban", ban, run_async=True))
+dispatcher.add_handler(CommandHandler("kick", kick, run_async=True))
+dispatcher.add_handler(CommandHandler("pin", pin, run_async=True))
+dispatcher.add_handler(CommandHandler("purge", purge, run_async=True))
