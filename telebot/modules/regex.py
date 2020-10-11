962c51b5ae6eb0bb8e0928b007116b47e9c07940
ksdfg <ksdfg123@gmail.com>
2020-10-11 12:53:14 +0530

    use sed via subprocess to replace text using regex
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/regex.py
+++ telebot/modules/regex.py
@@ -1,9 +1,44 @@
-from telegram import MAX_MESSAGE_LENGTH
+from re import compile
+from subprocess import Popen, PIPE, check_output, CalledProcessError, STDOUT
+
+from telegram import MAX_MESSAGE_LENGTH, Update
+from telegram.ext import CallbackContext, MessageHandler, Filters
+
+from telebot import dispatcher
+from telebot.functions import bot_action
+
+
+@bot_action("regex")
+def regex(update: Update, context: CallbackContext):
+    """
+    Replace text using sed and regex
+    :param update: object representing the incoming update.
+    :param context: object containing data about the command call.
+    """
+    if not update.effective_message.reply_to_message:
+        update.effective_message.reply_text("Gimme text to replace stuff in, baka!")
+
+    # make sure `s` command is properly terminated
+    command = update.effective_message.text
+    if command.count("/") == 2:
+        command += "/"
+
+    # execute sed in shell to get output
+    try:
+        text_input = Popen(("echo", update.effective_message.reply_to_message.text), stdout=PIPE, stderr=STDOUT)
+        result = check_output(("sed", "-r", command), stdin=text_input.stdout, stderr=STDOUT)
+    except CalledProcessError as e:
+        update.effective_message.reply_markdown(f"```{e.output.decode()}```")
+        return
+
+    # reply with the output text
+    update.effective_message.reply_to_message.reply_text(result.decode())
+
 
 __mod_name__ = "Regex"
 
 __help__ = f"""
-- `s/<text1>/<text2>[/<flags>]` : Reply to a message with this to perform a sed operation on that message, replacing all \
+- `s/<text1>/<text2>/[<flags>]` : Reply to a message with this to perform a sed operation on that message, replacing all \
 occurrences of 'text1' with 'text2'. Flags are optional, and currently include 'i' for ignore case, 'g' for global, \
 or nothing. Delimiters include `/`, `_`, `|`, and `:`. Text grouping is supported. The resulting message cannot be \
 larger than {MAX_MESSAGE_LENGTH} characters.
@@ -12,3 +47,6 @@ larger than {MAX_MESSAGE_LENGTH} characters.
 If you want to use these characters, make sure you escape them!
 eg: \\?.
 """
+
+# ad handlers
+dispatcher.add_handler(MessageHandler(Filters.regex(compile("s([/:|_]).*([/:|_]).*")), regex, run_async=True))
