d2fad1a888dfd5b440b568f3e3fdc159a81aacfd
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2020-11-21 00:01:33 +0530

    delete module (#43)
    * shift purge command to delete module
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * add /delete
    
    Co-authored-by: Paul Larsen <pn1315@ic.ac.uk>
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * return the value returned by the function in admin check wrappers
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * only admins can add and delete exceptions
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * use /del as well as /delete
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * Allow users to delete their own messages without the ability to delete messages.
    
    Also add a wrapper to check if a message has a reply_to_message.
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    * add /del to list of bot commands
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>
    
    Co-authored-by: Paul Larsen <pn1315@ic.ac.uk>

---
+++ telebot/modules/delete.py
@@ -0,0 +1,144 @@
+from functools import wraps
+from re import match
+from traceback import print_exc
+from typing import Callable
+
+from telegram import Update
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, CommandHandler
+
+from telebot import dispatcher
+from telebot.functions import (
+    check_user_admin,
+    check_bot_admin,
+    bot_action,
+    check_reply_to_message,
+)
+from telebot.modules.db.exceptions import get_command_exception_chats
+from telebot.modules.db.users import add_user
+
+
+def check_bot_can_delete(func: Callable):
+    """
+    Wrapper function for checking if bot can delete messages
+    :param func: The function this wraps over
+    """
+
+    @wraps(func)
+    def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
+        # check if bot has perms to delete a message
+        if (
+            update.effective_chat.type != "private"
+            and not update.effective_chat.get_member(context.bot.id).can_delete_messages
+        ):
+            update.effective_message.reply_text(
+                "Bribe your sugar daddy with some catnip and ask him to allow me to delete messages..."
+            )
+            return
+
+        return func(update, context, *args, **kwargs)
+
+    return wrapper
+
+
+def check_user_can_delete(func: Callable):
+    """
+    Wrapper function for checking if user can delete messages
+    :param func: The function this wraps over
+    """
+
+    @wraps(func)
+    def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
+        # check if user can delete messages
+        if (
+            update.effective_chat.type != "private"
+            and update.effective_message.reply_to_message.from_user.id != update.effective_user.id
+            and update.effective_chat.id not in get_command_exception_chats("delete")
+        ):
+            user = update.effective_chat.get_member(update.effective_user.id)
+            if not user.can_delete_messages and user.status != "creator":
+                update.effective_message.reply_markdown(
+                    "Ask your sugar daddy to give you perms required to use the method `CanDeleteMessages`, "
+                    "or add an exception to module `delete`."
+                )
+                return
+
+        return func(update, context, *args, **kwargs)
+
+    return wrapper
+
+
+@bot_action("delete")
+@check_reply_to_message(error_msg="I'm a cat, not a psychic! Reply to the message you want to delete...")
+@check_user_admin
+@check_user_can_delete
+@check_bot_admin
+@check_bot_can_delete
+def delete(update: Update, context: CallbackContext):
+    """
+    Delete the quoted message
+    :param update: object representing the incoming update.
+    :param context: object containing data about the command call.
+    """
+
+    # for future usage
+    add_user(
+        user_id=update.effective_message.reply_to_message.from_user.id,
+        username=update.effective_message.reply_to_message.from_user.username,
+    )
+
+    # delete message
+    try:
+        context.bot.delete_message(update.effective_chat.id, update.effective_message.reply_to_message.message_id)
+        context.bot.delete_message(update.effective_chat.id, update.effective_message.message_id)
+    except BadRequest as e:
+        if match("^Message can't be deleted", e.message):
+            print_exc()
+            update.effective_message.reply_text(
+                "I don't know why but I can't delete that...\nTelegram is high on catnip as usual."
+            )
+        else:
+            raise e
+
+
+@bot_action("purge")
+@check_user_admin
+@check_reply_to_message(error_msg="I'm a cat, not a psychic! Reply to the message you want to start deleting from...")
+@check_user_can_delete
+@check_bot_admin
+@check_bot_can_delete
+def purge(update: Update, context: CallbackContext):
+    """
+    Delete all messages from quoted message
+    :param update: object representing the incoming update.
+    :param context: object containing data about the command call.
+    """
+    # for future usage
+    add_user(
+        user_id=update.effective_message.reply_to_message.from_user.id,
+        username=update.effective_message.reply_to_message.from_user.username,
+    )
+
+    # delete messages
+    for id_ in range(update.effective_message.message_id, update.effective_message.reply_to_message.message_id - 1, -1):
+        try:
+            context.bot.delete_message(update.effective_chat.id, id_)
+        except BadRequest:
+            continue
+
+    update.effective_chat.send_message("Just like we do it in china....")
+
+
+__help__ = """
+- /del `<reply>` : delete the quoted message.
+- /purge `<reply>` : delete all messages from replied message to current one.
+
+If you add an exception to `delete`, I will allow admins to execute commands even if they don't have the permission to delete messages.
+"""
+
+__mod_name__ = "Delete"
+
+# create handlers
+dispatcher.add_handler(CommandHandler("del", delete, run_async=True))
+dispatcher.add_handler(CommandHandler("delete", delete, run_async=True))
+dispatcher.add_handler(CommandHandler("purge", purge, run_async=True))
