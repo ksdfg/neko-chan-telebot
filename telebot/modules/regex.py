d62c4be395f2a8f0b7d207dd0f35e65df6638e07
ksdfg <ksdfg123@gmail.com>
2020-11-23 19:42:01 +0530

    allow users to modify captions of messages as well as text
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/modules/regex.py
+++ telebot/modules/regex.py
@@ -20,7 +20,10 @@ def regex(update: Update, context: CallbackContext):
         return
 
     # check if there is a quoted message
-    if not (update.effective_message.reply_to_message and update.effective_message.reply_to_message.text):
+    if not (
+        update.effective_message.reply_to_message
+        and (update.effective_message.reply_to_message.text or update.effective_message.reply_to_message.caption)
+    ):
         update.effective_message.reply_text("Gimme text to replace stuff in, baka!")
         return
 
@@ -33,15 +36,52 @@ def regex(update: Update, context: CallbackContext):
     command = search(f"s{delimiter_regex_safe}.*{delimiter_regex_safe}.*{delimiter_regex_safe}[ig]*", command).group()
 
     # execute sed in shell to get output
+    content = (
+        update.effective_message.reply_to_message.text
+        if update.effective_message.reply_to_message.text
+        else update.effective_message.reply_to_message.caption
+    )
     try:
-        text_input = Popen(("echo", update.effective_message.reply_to_message.text), stdout=PIPE, stderr=STDOUT)
+        text_input = Popen(("echo", content), stdout=PIPE, stderr=STDOUT)
         result = check_output(("sed", "-r", command), stdin=text_input.stdout, stderr=STDOUT)
     except CalledProcessError as e:
         update.effective_message.reply_markdown(f"```{e.output.decode()}```")
         return
 
     # reply with the output text
-    update.effective_message.reply_to_message.reply_text(result.decode())
+    if update.effective_message.reply_to_message.document:
+        update.effective_message.reply_to_message.reply_document(
+            document=update.effective_message.reply_to_message.document,
+            filename=update.effective_message.reply_to_message.document.file_name,
+            caption=result.decode('utf-8'),
+            reply_markup=update.effective_message.reply_to_message.reply_markup,
+        )
+    elif update.effective_message.reply_to_message.photo:
+        update.effective_message.reply_to_message.reply_photo(
+            photo=update.effective_message.reply_to_message.photo[-1].file_id,
+            caption=result.decode('utf-8'),
+            reply_markup=update.effective_message.reply_to_message.reply_markup,
+        )
+    elif update.effective_message.reply_to_message.audio:
+        update.effective_message.reply_to_message.reply_audio(
+            photo=update.effective_message.reply_to_message.audio,
+            caption=result.decode('utf-8'),
+            reply_markup=update.effective_message.reply_to_message.reply_markup,
+        )
+    elif update.effective_message.reply_to_message.voice:
+        update.effective_message.reply_to_message.reply_voice(
+            photo=update.effective_message.reply_to_message.voice,
+            caption=result.decode('utf-8'),
+            reply_markup=update.effective_message.reply_to_message.reply_markup,
+        )
+    elif update.effective_message.reply_to_message.video:
+        update.effective_message.reply_to_message.reply_video(
+            photo=update.effective_message.reply_to_message.video,
+            caption=result.decode('utf-8'),
+            reply_markup=update.effective_message.reply_to_message.reply_markup,
+        )
+    else:
+        update.effective_message.reply_to_message.reply_text(result.decode('utf-8'))
 
 
 __mod_name__ = "Regex"
